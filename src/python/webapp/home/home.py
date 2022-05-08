2d580bc8d560cef0be912bd79b3a5dff297d0fda
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2022-05-08 17:57:48 +0300

    deploy home

---
+++ src/python/webapp/home/home.py
@@ -0,0 +1,11 @@
+from flask import Flask,jsonify
+import time
+app = Flask(__name__)
+
+@app.route("/2")
+def hello():
+    print('This should be in new image')
+    return jsonify("welcome home.")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True,port=5000)
