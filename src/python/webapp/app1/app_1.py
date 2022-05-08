780708fe0597aa13809bb357dc5e4f7732ffa687
finance-dataspider <78756320+finance-dataspider@users.noreply.github.com>
2022-05-08 17:49:06 +0300

    app1 update

--- src/python/webapp/app1/app_1.py
+++ src/python/webapp/app1/app_1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/1")
 def hello():
     print('This should be in new image')
-    return jsonify("app1!")
+    return jsonify("app1! update!")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5000)
